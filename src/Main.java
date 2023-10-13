fa8732613083aa52a9f559d20db5ca2c86f00684
alekseipeikhel <alekseipeikhel@gmail.com>
2023-10-13 15:14:42 +0300

    delete cycle

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,5 @@ public class Main {
         // IntelliJ IDEA suggests fixing it.
         System.out.println("Привет мир!");
         System.out.println("Hello world!");
-
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
